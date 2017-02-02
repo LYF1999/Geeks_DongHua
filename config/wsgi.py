7b173ed719ede9f1c760c3709d284c99e572e8d9
LYF1999 <850097832@qq.com>
2017-02-03 00:12:19 +0800

    fix
    fix

--- config/wsgi.py
+++ config/wsgi.py
@@ -8,9 +8,15 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
+from os.path import dirname, abspath
+
+PROJECT_DIR = dirname(dirname(abspath(__file__)))  # 3
+import sys  # 4
+
+sys.path.insert(0, PROJECT_DIR)  # 5
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings"
 
 application = get_wsgi_application()
