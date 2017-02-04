751d6bf2fa1b6226c084b4300b9545b4da7c7a2c
LYF1999 <850097832@qq.com>
2017-02-04 16:45:54 +0800

    fix
    fix

--- config/settings.py
+++ config/settings.py
@@ -133,6 +133,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, "collectstatic")
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 #  REST_FRAMEWORK
 REST_FRAMEWORK = {
--- config/wsgi.py
+++ config/wsgi.py
@@ -8,15 +8,12 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 """
 
 import os
-from os.path import dirname, abspath
-
-PROJECT_DIR = dirname(dirname(abspath(__file__)))  # 3
-import sys  # 4
-
-sys.path.insert(0, PROJECT_DIR)  # 5
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ["DJANGO_SETTINGS_MODULE"] = "config.settings"
 
 application = get_wsgi_application()
+
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
