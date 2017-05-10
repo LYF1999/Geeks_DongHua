e84efbb20f2dc91dcde31f8ac0de3ce330f16a8e
LYF1999 <lyfmagic99@gmail.com>
2017-05-10 08:20:49 +0800

    export
    export

---
+++ myuser/script/export_excel.py
@@ -0,0 +1,17 @@
+#  coding=utf-8
+from __future__ import unicode_literals
+from openpyxl import Workbook
+from myuser.models import User
+
+
+def export_member():
+    wb = Workbook()
+    new_ws = wb.create_sheet(title='students')
+    row = 1
+
+    users = User.objects.filter(is_member=True)
+
+    for user in users:
+        new_ws.cell(row=row, column=1).value = row
+        new_ws.cell(row=row, column=2).value = user.name
+        row += 1
